97b7e2cb9029abb29cec27728af7d05066030e7a
amirqodi <mmt154000@gmail.com>
2024-05-26 09:20:39 +0330

    message: signup form react-form-hook1.0

--- front-end/src/components/SignupForm.tsx
+++ front-end/src/components/SignupForm.tsx
@@ -1,7 +1,12 @@
 "use client"
 
 import { useState } from 'react';
-import SignupInput from './SignupInput';
+
+import {Controller,type FieldValue, useForm} from "react-hook-form"
+
+import { z } from "zod"
+import { zodResolver } from '@hookform/resolvers/zod';
+
 
 import DatePicker, { DateObject } from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
@@ -10,234 +15,320 @@ import gregorian from "react-date-object/calendars/gregorian";
 
 import cities from '../../public/cityName';
 
+import { FaCalendarAlt } from "react-icons/fa";
+
+
+const is_persian_string=(str: string): boolean => {
+  const persianPattern = /^[\u0600-\u06FF]+$/;
+
+  return persianPattern.test(str);
+}
+
+
+const userSchema = z.object({
+  username: z.string()
+    .regex(/^[a-zA-Z][a-zA-Z0-9_]{4,20}$/, "نام کاربری معتبر نمیباشد")
+    .min(1,"نام کاربری نمی‌تواند خالی باشد"),
+
+  password: z.string()
+    .min(1,"گذرواژه نمی‌تواند خالی باشد")
+    .min(8, "گذرواژه باید حداقل 8 کاراکتر باشد.")
+    .regex(/[A-Z]/, "گذرواژه باید حداقل شامل یک حرف بزرگ انگلیسی باشد.")
+    .regex(/[0-9]/, "گذرواژه باید حداقل شامل یک عدد باشد.")
+    .regex(/[!@#$%^&*(),.?":{}|<>]/, "گذرواژه باید حداقل شامل یک کاراکتر ویژه باشد."),
+
+  confirmPassword: z.string()
+    .min(1,"تکرار گذرواژه نمی‌تواند خالی باشد"),
+
+  firstName: z.string({ required_error: "نام نمی‌تواند خالی باشد" })
+    .refine(is_persian_string, "نام شما باید فارسی باشد"),
+  lastName: z.string({ required_error: "نام خانوادگی نمی‌تواند خالی باشد" })
+    .refine(is_persian_string, "نام خانوادگی شما باید فارسی باشد"),
+
+  email: z.string({required_error:"ایمیل نمی‌تواند خالی باشد"})
+    .email("ایمیل وارد شده معتبر نمی‌باشد"),
+
+  phoneNumber: z.string()
+    .regex(/^\+\d{9,15}$/, "شماره تلفن وارد شده معتبر نمی‌باشد فرمت درست : ...98+")
+    .min(1,"شماره تلفن نمی‌تواند خالی باشد"),
+
+  landlineNumber: z.string()
+    .regex(/^\+\d{9,15}$/, "شماره ثابت وارد شده معتبر نمی‌باشد فرمت درست : ...98+")
+    .min(1,"شماره ثابت نمی‌تواند خالی باشد"),
+  city: z.string({required_error: "شهر نمی‌تواند خالی باشد"}),
+
+})
+.refine((data) => data.password === data.confirmPassword, {
+  message: "رمزعبور یکسان نیست",
+  path: ["confirmPassword"]
+});
+
+type signUpSchema = z.infer<typeof userSchema>;
+
+
+
 
 
 
 const SignupForm = () => {
 
-  const validateUsername = (username:string) => {
-    const pattern = /^[a-zA-Z][a-zA-Z0-9_]{4,20}$/;
-    if (username.length>1 &&  !pattern.test(username)) {
-      return "نام کاربری معتبر نمیباشد"
-    }
-
-    return "";
-  };
-  
-  const validatePassword = (password:string) => {
-    if (password.length > 1 && password.length < 8) {
-      return "گذرواژه باید حداقل 8 کاراکتر باشد.";
-    }
-    if (password.length > 1 && !/[A-Z]/.test(password) ) {
-      return "گذرواژه باید حداقل شامل یک حرف بزرگ انگلیسی باشد.";
-    }
-    if (password.length > 1 && !/[0-9]/.test(password)) {
-      return "گذرواژه باید حداقل شامل یک عدد باشد.";
-    }
-    if (password.length > 1 && !/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
-      return "گذرواژه باید حداقل شامل یک کاراکتر ویژه باشد.";
-    }
-    return "";
-  };
-  
-  const isPersianString = (str:string) => {
-    const persianPattern = /^[\u0600-\u06FF\s]+$/;
-    return persianPattern.test(str);
-  };
-  
-  const validateFirstName = (firstName:string) => {
-    if (firstName.length > 1 && !isPersianString(firstName)) {
-      return "نام شما باید فارسی باشد";
-    }
-    return "";
-  };
-  
-  const validateLastName = (lastName:string) => {
-    if (lastName.length > 1 && !isPersianString(lastName)) {
-      return "نام خانوادگی شما باید فارسی باشد";
-    }
-    return "";
-  };
-  
 
 
-  const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-    first_name: '',
-    last_name: '',
-    phone_number: '',
-    landline_number: '',
-    email: '',
-    city: '',
-    birthDate: null as String | null,
-  });
-
-
-  const [bdate, setBdate] = useState<DateObject | null>(null);
-  const [cPass, setcPass] = useState("")
-
-  const [userNameError, setUserNameError] = useState("")
-  const [passError, setPassError] = useState("")
-  const [firstNameError, setFirstNameError] = useState("")
-  const [lastNameError, setlastNameError] = useState("")
-
+  const [bdate, setBdate] = useState< DateObject | null>(null);
+  const [birthDate, setBirthDate] = useState<String | null>(null);
   const handleDateChange = (date: DateObject | null) => {
     setBdate(date);
     const gregorianDate = date ? date.convert(gregorian) : null;
-    setFormData({
-      ...formData,
-      birthDate: gregorianDate ? gregorianDate.toDate().toISOString().split("T")[0]: null
-    });
+    setBirthDate(gregorianDate ? gregorianDate.toDate().toISOString().split("T")[0]: null)
   };
 
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-
-    setUserNameError(validateUsername(formData.username))
-    setPassError(validatePassword(formData.password))
-    setFirstNameError(validateFirstName(formData.first_name))
-    setlastNameError(validateLastName(formData.last_name))
-  };
-
-  const handleSubmit = async (e : React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const response = await fetch('http://127.0.0.1:8000/users/graphql/', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    query: `
-    mutation {
-        createUser(
-          userData: {
-            username: "${formData.username}",
-            firstName: "${formData.first_name}",
-            lastName: "${formData.last_name}",
-            password: "${formData.password}",
-            phoneNumber: "${formData.phone_number}",
-            landlineNumber: "${formData.landline_number}",
-            email: "${formData.email}",
-            city: "${formData.city}",
-            birthDate: "${formData.birthDate}"
-          }
-        ) {
-          success
-          errors
-        }
+  const onSubmit =async (userInfo:signUpSchema)=>{
+    try {
+      const response = await fetch('http://127.0.0.1:8000/users/graphql/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: `
+            mutation {
+              createUser(
+                userData: {
+                  username: "${userInfo.username}",
+                  firstName: "${userInfo.firstName}",
+                  lastName: "${userInfo.lastName}",
+                  password: "${userInfo.password}",
+                  phoneNumber: "${userInfo.phoneNumber}",
+                  landlineNumber: "${userInfo.landlineNumber}",
+                  email: "${userInfo.email}",
+                  city: "${userInfo.city}",
+                  birthDate: "${birthDate}"
+                }
+              ) {
+                success
+                errors
+              }
+            }
+          `,
+        }),
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        alert("failed")
       }
-    `,
-  }),
-});
-    const data = await response.json();
-    console.log(data);
-    console.log(formData)
-  };
+      console.log(data.errors);
+      
+      if (data.errors) {
+        const errors = data.errors;
+         if (errors.username) {
+          setError("username", {
+            type: "server",
+            message: errors.username
+          })
+         }
+         if (errors.password) {
+          setError("password", {
+            type: "server",
+            message: errors.password
+          })
+         }
+         if (errors.email) {
+          setError("email", {
+            type: "server",
+            message: errors.email
+          })
+         }
+         if (errors.phone_number) {
+          setError("phoneNumber", {
+            type: "server",
+            message: errors.phone_number
+          })
+         }
+         if (errors.landlineNumber) {
+          setError("landlineNumber", {
+            type: "server",
+            message: errors.landlineNumber
+          })
+         }
+      }
+      console.log(data);
+    } catch (error) {
+      console.error("Error submitting the form:", error);
+    }
+    // reset()
+     
+  }
 
+    const {
+      register,
+      handleSubmit,
+      formState:{errors,isSubmitting},
+      setError,
+      reset,
+    } = useForm<signUpSchema>({
+      resolver:zodResolver(userSchema)
+    })
 
   return (
-    <form onSubmit={handleSubmit} className='signup_form flex flex-col items-center w-4/5 m-auto p-10 '>
-      <SignupInput
-        title="نام کاربری "
-        type="text"
-        id="username"
-        name="username"
-        value={formData.username}
-        onChange={handleChange}
-        required={true}
-        verror={userNameError}
-      />
-      <SignupInput
-        dir="ltr"
-        title="رمز عبور "
-        type="password"
-        id="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-        required={true}
-        verror={passError}
-      />
+    <form noValidate  onSubmit={handleSubmit(onSubmit)} className='signup_form flex flex-col items-start w-[80%]   m-auto p-10 '>
 
-     <SignupInput
-        dir="ltr"
-        title="تکرار رمز عبور "
-        type="password"
-        id="repeat-password"
-        name="repeat-password"
-        value={cPass}
-        onChange={(e)=>setcPass(e.target.value)}
-        required={true}
+<div className='signup_form_container'>
+  <input
+    {...register("username", {
+      required: "نام کاربری الزامی است",
+    })}
+    className="signup_form_input"
+    id='user-name'
+    type='text'
+    required
+  />
+    <label className="signup_form_label" htmlFor="user-name">نام کاربری :</label>
+  <div className='line'></div>
+  {errors.username && (
+    <p className='text-sm text-red-700 absolute bottom-0 translate-y-5'>
+      {`${errors.username.message}`}
+    </p>
+  )}
+</div>
+
+
+
+
+
+      <div className='signup_form_container'>
+      <input
+        {...register("password",{
+          required:"رمز عبور الزامی است",
+        })}
+        className="signup_form_input"
+        id='password'
+        dir='ltr'
+        type='password'
+        required
       />
+      {errors.password &&( 
+        <p className='text-sm text-red-700 absolute bottom-0 translate-y-5'>{`${errors.password.message}`}</p>
+      )}
+      <span className='line'></span>
+      <label className="signup_form_label" htmlFor="password">رمز عبور :</label>
+    </div>
+
+    <div className='signup_form_container'>
+      <input
+        {...register("confirmPassword")}
+        className="signup_form_input"
+        id='confirmPassword'
+        dir='ltr'
+        type='password'
+        required
+      />
+      {errors.confirmPassword &&( 
+        <p className='text-sm text-red-700 absolute bottom-0 translate-y-5'>{`${errors.confirmPassword.message}`}</p>
+      )}
+      <span className='line'></span>
+      <label className="signup_form_label" htmlFor="confirmPassword">تکرار رمز عبور :</label>
+    </div>
+
+    <div className='signup_form_container'>
+      <input
+        {...register("firstName")}
+        className="signup_form_input"
+        id='firstName'
+        type='text'
+        required
+      />
+      {errors.firstName &&( 
+        <p className='text-sm text-red-700 absolute bottom-0 translate-y-5'>{`${errors.firstName.message}`}</p>
+      )}
+      <span className='line'></span>
+      <label className="signup_form_label" htmlFor="firstName">نام :</label>
+    </div>
+
+    <div className='signup_form_container'>
+      <input
+        {...register("lastName")}
+        className="signup_form_input"
+        id='lastName'
+        type='text'
+        required
+      />
+      {errors.lastName &&( 
+        <p className='text-sm text-red-700 absolute bottom-0 translate-y-5'>{`${errors.lastName.message}`}</p>
+      )}
+      <span className='line'></span>
+      <label className="signup_form_label" htmlFor="lastName">نام خانوادگی :</label>
+    </div>
       
 
-      <SignupInput
-        title="نام "
-        type="text"
-        id="first-name"
-        name="first_name"
-        value={formData.first_name}
-        onChange={handleChange}
-        required={true}
-        verror={firstNameError}
-      />
 
-      <SignupInput
-        title="نام خانوادگی "
-        type="text"
-        id="last-name"
-        name="last_name"
-        value={formData.last_name}
-        onChange={handleChange}
-        required={true}
-        verror={lastNameError}
-      />
 
-      <SignupInput
-        dir="ltr"
-        title="شماره تلفن همراه "
-        type="tel"
-        id="phone-number"
-        name="phone_number"
-        value={formData.phone_number}
-        onChange={handleChange}
-        required={true}
-      />
 
-      <SignupInput
-        dir="ltr"
-        title="تلفن ثابت "
-        type="tel"
-        id="landline-number"
-        name="landline_number"
-        value={formData.landline_number}
-        onChange={handleChange}
-        required={true}
-      />
 
-      <SignupInput
-        dir="ltr"
-        title="ایمیل"
-        type="email"
-        id="email"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-        required={true}
+    <div className='signup_form_container'>
+      <input
+        {...register("email")}
+        className="signup_form_input"
+        id='email'
+        dir='ltr'
+        type='email'
+        required
       />
+      {errors.email &&( 
+        <p className='text-sm text-red-700 absolute bottom-0 translate-y-5'>{`${errors.email.message}`}</p>
+      )}
+      <span className='line'></span>
+      <label className="signup_form_label" htmlFor="email">ایمیل :</label>
+    </div>
 
-     <div className='m-4 w-1/3 flex flex-col'>
+    <div className='signup_form_container'>
+      <input
+        {...register("phoneNumber")}
+        className="signup_form_input"
+        id='phoneNumber'
+        dir='ltr'
+        type='tel'
+        required
+      />
+      {errors.phoneNumber &&( 
+        <p className='text-sm text-red-700 absolute bottom-0 translate-y-5'>{`${errors.phoneNumber.message}`}</p>
+      )}
+      <span className='line'></span>
+      <label className="signup_form_label" htmlFor="phoneNumber">شماره تلفن :</label>
+    </div>
+
+    <div className='signup_form_container'>
+      <input
+        {...register("landlineNumber")}
+        className="signup_form_input"
+        id='landlineNumber'
+        dir='ltr'
+        type='tel'
+        required
+      />
+      {errors.landlineNumber &&( 
+        <p className='text-sm text-red-700 absolute bottom-0 translate-y-5'>{`${errors.landlineNumber.message}`}</p>
+      )}
+      <span className='line'></span>
+      <label className="signup_form_label" htmlFor="landlineNumber">شماره تلفن ثابت :</label>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+     <div className='signup_form_container'>
         <label htmlFor="city">
-           شهر
+           شهر :
         </label>
-        <select className='text-black' name="city" id="city" onChange={handleChange} value={formData.city} required>
+        <select className='text-black' id="city" {...register("city")} required>
         <option value={""} disabled>انتخاب کنید</option>
             {
                 cities.map((value, index)=>{
@@ -249,22 +340,26 @@ const SignupForm = () => {
         </select>
       </div>
 
-      <div className='m-4 w-1/3 flex flex-col'>
-        <label htmlFor="birthDate">تاریخ تولد :</label>
-        <DatePicker
-          calendar={persian}
-          locale={persian_fa}
-          calendarPosition="bottom-right"
-          value={bdate}
-          onChange={handleDateChange}
-          maxDate={new DateObject({ calendar: persian }).set("day", 15)}
-          required
-        />
-      </div>
+      <div className='flex justify-start mt-4 gap-2'>
+  <label htmlFor="birthDate" className='flex items-center gap-2'>   تاریخ تولد :<FaCalendarAlt /></label>
+
+      <DatePicker
+        id='birthDate'
+        calendar={persian}
+        locale={persian_fa}
+        calendarPosition="bottom-right"
+        value={bdate}
+        onChange={handleDateChange}
+        maxDate={new DateObject({ calendar: persian }).set("day", 15)}
+        required
+      />
+</div>
+
 
       <button 
+        disabled={isSubmitting}
         type='submit'
-        className="w-1/8 py-2 mt-4 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
+        className="w-1/8 py-2 mt-4 bg-red-600 text-[#212121] rounded hover:bg-red-700 focus:outline-none disabled:bg-red-300"
       >
         ثبت نام
       </button>
@@ -273,7 +368,5 @@ const SignupForm = () => {
 };
 
 export default SignupForm;
-function moment(arg0: Date) {
-    throw new Error('Function not implemented.');
-}
+
 
--- front-end/src/components/SignupInput.tsx
+++ front-end/src/components/SignupInput.tsx
@@ -1,4 +1,4 @@
-import { error } from 'console';
+
 import React from 'react';
 
 interface SignupInputProps {
@@ -11,6 +11,7 @@ interface SignupInputProps {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   required?: boolean;
   verror?: string;
+  onRegionChange?:(value: string) => void;
 }
 
 const SignupInput: React.FC<SignupInputProps> = ({
@@ -21,21 +22,25 @@ const SignupInput: React.FC<SignupInputProps> = ({
   name,
   value,
   onChange,
-  required = false,
-  verror
+  required,
+  verror,
+  onRegionChange
 }) => {
+
+
+  const handleRegionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+    if (onRegionChange) {
+      onRegionChange(value);
+    }
+  };
+
   return (
-    <div className='m-4 w-1/3 flex flex-col relative'>
-      <label className='block' htmlFor={id}>{title}:</label>
+    <div className='signup_form_container'>
+      <label className={value?"absolute top-1/2 right-[1%] translate-y-[-150%]":`signup_form_label`} htmlFor={id}>{title}:</label>
       <input
         dir={dir}
-        className='bg-transparent text-white border-2 border-zinc-500 outline-0 p-1 border rounded
-        focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-      invalid:border-red-500 invalid:text-red-600
-      focus:invalid:border-red-500 focus:invalid:ring-red-500
-        
-        '
+        className={type==="tel"?'signup_form_input !pl-[10%]':"signup_form_input"}
         type={type}
         id={id}
         name={name}
@@ -43,9 +48,24 @@ const SignupInput: React.FC<SignupInputProps> = ({
         onChange={onChange}
         required={required}
       />
-      <span className='text-sm text-red-700'>{verror?`${verror}*`:""}</span>
+      <span className='line'></span>
+      <span className='text-sm text-red-700 absolute bottom-0 translate-y-5'>{verror?`${verror}*`:""}</span>
+      {type==="tel"?
+        <select onChange={handleRegionChange} className='w-[10%] h-50px absolute left-0 top-[50%] translate-y-[-50%] pb-[.75%] bg-inherit text-color-white-700 ' name="region" id="region">
+        <option className='text-black' value="+98">98+</option>
+        <option className='text-black' value="+96">96+</option>
+      </select>:""  
+    }
+      
     </div>
   );
 };
 
 export default SignupInput;
+
+
+
+
+
+
+
