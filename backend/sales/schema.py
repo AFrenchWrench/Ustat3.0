139ab5f269b147ffee7330a071ca76f3aaae9290
AFrenchWrench <AFrenchWrench@gmail.com>
2024-07-11 17:22:05 +0330

    added update mutations

--- backend/sales/schema.py
+++ backend/sales/schema.py
@@ -9,6 +9,7 @@ from graphql import (
 )
 from utils.schema_utils import (
     login_required,
+    staff_member_required,
 )
 from sales.models import (
     Order,
@@ -64,7 +65,8 @@ class CreateOrderItem(graphene.Mutation):
 
     order_item = graphene.Field(OrderItemType)
     success = graphene.Boolean()
-    
+
+    @login_required
     def mutate(self, info, input):
         try:
             if input.get("order"):
@@ -75,10 +77,11 @@ class CreateOrderItem(graphene.Mutation):
                 **input,
             )
 
-            return CreateOrderItem(order_item=order_item,success=True)
+            return CreateOrderItem(order_item=order_item, success=True)
         except Exception as e:
             print(e)
             return CreateOrderItem(success=False)
+
 
 class CreateDisplayItem(graphene.Mutation):
     class Arguments:
@@ -86,7 +89,8 @@ class CreateDisplayItem(graphene.Mutation):
 
     display_item = graphene.Field(DisplayItemType)
     success = graphene.Boolean()
-    
+
+    @staff_member_required
     def mutate(self, info, input):
         try:
             display_item = DisplayItem.objects.create(
@@ -96,7 +100,7 @@ class CreateDisplayItem(graphene.Mutation):
         except Exception as e:
             print(e)
             return CreateDisplayItem(success=False)
-        
+
 
 # ========================Create End========================
 
@@ -104,24 +108,76 @@ class CreateDisplayItem(graphene.Mutation):
 # ========================Update Start========================
 
 
+class UpdateOrderItemInput(graphene.InputObjectType):
+    id = graphene.ID(required=True)
+    due_date = graphene.Date(required=False)
+    status = graphene.String(required=False)
+    dimensions = graphene.JSONString(required=False)
+    description = graphene.String(required=False)
+    quantity = graphene.Int(required=False)
+
+
+class DisplayItemInput(graphene.InputObjectType):
+    id = graphene.ID(required=True)
+    type = graphene.String(required=False)
+    name = graphene.String(required=False)
+    dimensions = graphene.JSONString(required=False)
+    price = graphene.BigInt(required=False)
+    description = graphene.String(required=False)
+
+
+class UpdateOrderItem(graphene.Mutation):
+    class Arguments:
+        input = OrderItemInput(required=True)
+
+    order_item = graphene.Field(OrderItemType)
+    success = graphene.Boolean()
+
+    @login_required
+    def mutate(self, info, input):
+        try:
+            order_item = get_object_or_404(OrderItem, pk=input.get("id"))
+            for field, value in input:
+                if field == "due_date" or field == "status":
+                    setattr(order_item.order, field, value)
+                    order_item.order.save()
+                else:
+                    setattr(order_item, field, value)
+            order_item.save()
+            return UpdateOrderItem(order_item=order_item, success=True)
+        except Exception as e:
+            print(e)
+            return UpdateOrderItem(success=False)
+
+
+class UpdateDisplayItem(graphene.Mutation):
+    class Arguments:
+        input = DisplayItemInput(required=True)
+
+    display_item = graphene.Field(DisplayItemType)
+    success = graphene.Boolean()
+
+    @staff_member_required
+    def mutate(self, info, input):
+        try:
+            display_item = get_object_or_404(DisplayItem, pk=input.get("id"))
+            for field, value in input:
+                setattr(display_item, field, value)
+            display_item.save()
+            return UpdateDisplayItem(display_item=display_item, success=True)
+        except Exception as e:
+            print(e)
+            return UpdateDisplayItem(success=False)
+
+
 # ========================Update End========================
-
-
-# ========================Verification Start========================
-
-
-# ========================Verification End========================
-
-
-# ========================Auth Start========================
-
-
-# ========================Auth End========================
 
 
 class Mutation(graphene.ObjectType):
     create_order_item = CreateOrderItem.Field()
     create_display_item = CreateDisplayItem.Field()
+    update_order_item = UpdateOrderItem.Field()
+    update_display_item = UpdateDisplayItem.Field()
 
 
 # ========================Mutations End========================
