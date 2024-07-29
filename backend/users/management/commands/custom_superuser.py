f100dbaf767abb7c3db905d671ae994ca14e35ff
AFrenchWrench <AFrenchWrench@gmail.com>
2024-07-29 18:05:57 +0330

    fixed cities and exposed db

--- backend/users/management/commands/custom_superuser.py
+++ backend/users/management/commands/custom_superuser.py
@@ -2,17 +2,22 @@ from decouple import config
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
+
+from main.models import Cities
 
 
 class Command(BaseCommand):
-    help = 'Create a superuser with custom fields'
+    help = "Create a superuser with custom fields"
 
     def handle(self, *args, **kwargs):
         try:
             User = get_user_model()
             try:
-                user = User.objects.get(username=config('SUPERUSERNAME'))
-                self.stdout.write(self.style.ERROR(f'User with the username {user.username} exists!'))
+                user = User.objects.get(username=config("SUPERUSERNAME"))
+                self.stdout.write(
+                    self.style.ERROR(f"User with the username {user.username} exists!")
+                )
             except User.DoesNotExist:
                 user = User.objects.create_superuser(
                     username=config("SUPERUSERNAME"),
@@ -23,7 +28,12 @@ class Command(BaseCommand):
                     last_name=config("LAST_NAME"),
                     birthdate=config("BIRTHDATE"),
                     password=config("PASSWORD"),
+                    city=get_object_or_404(Cities, name=config("CITY")),
                 )
-                self.stdout.write(self.style.SUCCESS(f'Superuser {user.username} created successfully'))
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"Superuser {user.username} created successfully"
+                    )
+                )
         except ValidationError as e:
-            self.stderr.write(self.style.ERROR(f'Error: {e}'))
+            self.stderr.write(self.style.ERROR(f"Error: {e}"))
